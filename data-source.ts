ba5591abf0be2df3360486d891f7a170570590ec
engmhisham <eng.mhisham96@gmail.com>
2025-04-27 22:51:22 +0300

    handle migrations v2

--- data-source.ts
+++ data-source.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export default new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: +process.env.DB_PORT!,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
