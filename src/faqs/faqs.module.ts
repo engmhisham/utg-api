b59618f9adf5c9dda932d4c70dfb3da7ce990bd7
engmhisham <eng.mhisham96@gmail.com>
2025-05-05 21:11:14 +0300

    test4

--- src/faqs/faqs.module.ts
+++ src/faqs/faqs.module.ts
@@ -4,11 +4,13 @@ import { FaqsController } from './faqs.controller';
 import { FaqsService } from './faqs.service';
 import { Faq } from './entities/faq.entity';
 import { AuditLogsModule } from '../audit-logs/audit-logs.module';
+import { CategoriesModule } from '../categories/categories.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Faq]),
     AuditLogsModule,
+    CategoriesModule,
   ],
   controllers: [FaqsController],
   providers: [FaqsService],
